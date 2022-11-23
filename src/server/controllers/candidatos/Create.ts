ee8e9dbc7755810d5f6babff903faa5db339edb9
JulioTomazoni <juliotomazoni18@gmail.com>
2022-11-22 23:40:39 -0300

    feat: Create Controllers

--- src/server/controllers/candidatos/Create.ts
+++ src/server/controllers/candidatos/Create.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
-import { string } from 'yup/lib/locale';
+
+import { validation } from '../../shared/middlewares';
 
 interface ICandidato {
   nome: string;
@@ -9,32 +10,17 @@ interface ICandidato {
   cargo: string;
 }
 
-const bodyValidate: yup.SchemaOf<ICandidato>  = yup.object().shape({
-  nome: yup.string().required().min(3),
-  partido: yup.string().required().max(6),
-  cargo: yup.string().required(),
-});
-
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICandidato>(yup.object().shape({
+    nome: yup.string().required().min(3),
+    partido: yup.string().required().max(6),
+    cargo: yup.string().required(),
+  })),
+}));
 
 export const create = async (req: Request<{}, {}, ICandidato>, res: Response) => {
-  let validatedData: ICandidato | undefined = undefined;
-  
 
-  try {
-    validatedData = await bodyValidate.validate(req.body, { abortEarly : false });
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
+  console.log(req.body);
 
-    yupError.inner.forEach(error => {
-      if (!error.path) return; 
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: errors,
-    });
-  }  
-
-  console.log(validatedData);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado');
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
-
 import { CandidatosController } from './../controllers';
 
 const router = Router();
 
 router.get('/', (_, res) => {
-  return res.send('Funcionando!');
+  return res.send('Seja Bem-Vindo!');
 });
 
-router.post('/candidatos', CandidatosController.create);
-
-
-
-
-
+router.get('/candidatos', CandidatosController.getAllValidation, CandidatosController.getAll);
+router.post('/candidatos', CandidatosController.createValidation, CandidatosController.create);
+router.get('./candidatos/:id', CandidatosController.getByIdValidation, CandidatosController.getById);
+router.put('./candidatos/:id', CandidatosController.updateByIdValidation, CandidatosController.updateById);
+router.delete('./candidatos/:id', CandidatosController.deleteByIdValidation, CandidatosController.deleteById);
 
 export { router };
